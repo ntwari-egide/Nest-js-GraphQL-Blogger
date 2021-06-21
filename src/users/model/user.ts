2d777e11bd362bfa91344a896e415a48169ea6ff
ntwari <ntwariegide2@gmail.com>
2021-06-21 14:12:31 +0200

    fix bug in create user

--- src/users/model/user.ts
+++ src/users/model/user.ts
@@ -1,6 +1,18 @@
+import { Field, Int, ObjectType } from "@nestjs/graphql"
+import { IsOptional } from "class-validator"
+
+@ObjectType()
 export class User {
+    @Field()
     userId: String
+
+    @Field()
     email: String
+
+    @Field(()=> Int)
     age: Number
+
+    @Field({ nullable: true })
+    @IsOptional()
     isSubscribed: boolean
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { UsersResolver } from './resolver/users.resolver';
 import { UsersService } from './users.service';
 
 @Module({
-  providers: [UsersService]
+  providers: [UsersService,UsersResolver]
 })
 export class UsersModule {}
