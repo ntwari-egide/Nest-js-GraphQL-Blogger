f35e05f7ba07df484b2c48a584a9fa955b00bd70
ntwari <ntwariegide2@gmail.com>
2021-06-21 13:25:37 +0200

    naming query

--- src/users/resolver/users.resolver.ts
+++ src/users/resolver/users.resolver.ts
@@ -1,8 +1,23 @@
-import { Resolver } from "@nestjs/graphql";
+import { Query, Resolver } from "@nestjs/graphql";
 import { User } from "../model/user";
+import { UsersService } from "../users.service";
 
 // resolver takes the functino that return the Type resolver is going to handler
 @Resolver(()=> User)
 export class UsersResolver{
 
+    // resolver must return the data from the services like controllers do
+
+    constructor(private readonly usersService: UsersService){}
+
+
+    @Query(()=> User, { name: 'user', nullable: true })
+    getUser(): User{
+        return this.usersService.getUser()
+    }
+
+}
+
+function nullable(arg0: () => typeof User, name: void, arg2: string, nullable: any, arg4: boolean) {
+    throw new Error("Function not implemented.");
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,4 +11,8 @@ export class UsersService {
         return 
     }
 
+    public getUser(): User{
+        return 
+    }
+
 }
