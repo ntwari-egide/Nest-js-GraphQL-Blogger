ee59623e8c37203c9477f093153a16ef24a64228
ntwari <ntwariegide2@gmail.com>
2021-06-21 13:41:02 +0200

    created create user mutation

--- src/users/resolver/users.resolver.ts
+++ src/users/resolver/users.resolver.ts
@@ -1,5 +1,6 @@
-import { Args, Query, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { GetUserArgs } from "../dto/args/get-user.args";
+import { GetUsersArgs } from "../dto/args/get-users.args";
 import { User } from "../model/user";
 import { UsersService } from "../users.service";
 
@@ -24,10 +25,15 @@ export class UsersResolver{
      * Nullable items means that we are accepting know items only not lists
      */
     @Query(()=> [User] , { name: 'users', nullable: 'items'})
-    getUsers(): [User]{
+    getUsers(@Args() getUsersArgs : GetUsersArgs): [User]{
+
         return this.usersService.getUsers()
     }
 
+    @Mutation(()=> User)
+    createUser(): User{
+        return this.usersService.createUser()
+    }
     
 
 }
