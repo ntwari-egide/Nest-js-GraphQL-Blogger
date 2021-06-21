7a15b85b78f7da572cb6d24261ee2796ccc3a1fc
ntwari <ntwariegide2@gmail.com>
2021-06-21 13:10:25 +0200

    addubg users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({})
+    GraphQLModule.forRoot({
+      debug: false,
+      playground: false,
+      autoSchemaFile: join(process.cwd(),'src/schema.gql'),
+      sortSchema: true
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
